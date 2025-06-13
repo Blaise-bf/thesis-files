ca74ddc6da7040ef26a98d7a83ac05367a838be1
Blaise-bf <63640690+Blaise-bf@users.noreply.github.com>
2025-06-14 01:12:20 +0200

    Update models.py

--- models.py
+++ models.py
@@ -38,61 +38,61 @@ def masking(in_channels):
                             )
 
    
-class UNet3plus(nn.Module):
+# class UNet3plus(nn.Module):
 
-    def __init__(self, num_classes, encoder = "pvt_v2_b2_li"):
-        super().__init__()
+#     def __init__(self, num_classes, encoder = "pvt_v2_b2_li"):
+#         super().__init__()
                             
-        enc =  timm.create_model(encoder, pretrained = True)
+#         enc =  timm.create_model(encoder, pretrained = True)
 
-        self.econv0 = enc.patch_embed #64   ,256X256
-        self.econv1 = enc.stages[0] #64    ,256X256
-        self.econv2 = enc.stages[1]
-        self.econv3 = enc.stages[2]
-        self.econv4 = enc.stages[3]
+#         self.econv0 = enc.patch_embed #64   ,256X256
+#         self.econv1 = enc.stages[0] #64    ,256X256
+#         self.econv2 = enc.stages[1]
+#         self.econv3 = enc.stages[2]
+#         self.econv4 = enc.stages[3]
         
-        self.dconv4 =  double_conv(512, 256)  #,
-        self.single4 = single_conv(768,256)
+#         self.dconv4 =  double_conv(512, 256)  #,
+#         self.single4 = single_conv(768,256)
         
-        self.dconv3 =  double_conv(256, 128)
-        self.single3 = single_conv(320,256)
+#         self.dconv3 =  double_conv(256, 128)
+#         self.single3 = single_conv(320,256)
         
-        self.dconv2 = double_conv(256, 64)
-        self.dconv1 = double_conv(128, 32)
-        #self.dconv0 = double_conv(64+32, 32)
-        
-        
-        self.maxpool = nn.MaxPool2d(2)
-        self.upsample = nn.Upsample(scale_factor=2, mode = 'bilinear', align_corners=True)
-        self.conv_last = nn.Conv2d(32, num_classes, 1)
+#         self.dconv2 = double_conv(256, 64)
+#         self.dconv1 = double_conv(128, 32)
+#         #self.dconv0 = double_conv(64+32, 32)
         
         
-    def forward(self, x):
-        #x 1024X1021       
-        conv0 = self.econv0(x) # 512X512
-        conv1 = self.econv1(conv0[0], feat_size = conv0[1])# 256X256
-        conv2 = self.econv2(conv1[0], feat_size = conv1[1])
-        conv3 = self.econv3(conv2[0], feat_size = conv2[1])# 64X64
-        conv4 = self.econv4(conv3[0], feat_size = conv3[1])# 32X32
+#         self.maxpool = nn.MaxPool2d(2)
+#         self.upsample = nn.Upsample(scale_factor=2, mode = 'bilinear', align_corners=True)
+#         self.conv_last = nn.Conv2d(32, num_classes, 1)
         
         
-        dec4 = self.dconv4(conv4[0])
-        dec4 = torch.cat([self.upsample(dec4), conv3[0], self.maxpool(conv2[0]), self.maxpool(self.maxpool(conv1[0]))], dim=1)# 64X64
+#     def forward(self, x):
+#         #x 1024X1021       
+#         conv0 = self.econv0(x) # 512X512
+#         conv1 = self.econv1(conv0[0], feat_size = conv0[1])# 256X256
+#         conv2 = self.econv2(conv1[0], feat_size = conv1[1])
+#         conv3 = self.econv3(conv2[0], feat_size = conv2[1])# 64X64
+#         conv4 = self.econv4(conv3[0], feat_size = conv3[1])# 32X32
         
-        dec4 = self.single4(dec4)
         
-        dec3 = self.dconv3(dec4)
-        #dec3 = torch.cat([self.upsample(dec3), conv2[0]], dim=1)# 128X128
-        dec3 = torch.cat([self.upsample(dec3), conv2[0],self.maxpool(conv1[0])], dim=1)# 128X128
+#         dec4 = self.dconv4(conv4[0])
+#         dec4 = torch.cat([self.upsample(dec4), conv3[0], self.maxpool(conv2[0]), self.maxpool(self.maxpool(conv1[0]))], dim=1)# 64X64
         
-        dec3 = self.single3(dec3)
+#         dec4 = self.single4(dec4)
         
-        dec2 = self.dconv2(dec3)
-        dec2 = torch.cat([self.upsample(dec2), conv1[0]], dim=1)# 256X256
-        dec1 = self.dconv1(dec2)
-        out = self.conv_last(dec1)
+#         dec3 = self.dconv3(dec4)
+#         #dec3 = torch.cat([self.upsample(dec3), conv2[0]], dim=1)# 128X128
+#         dec3 = torch.cat([self.upsample(dec3), conv2[0],self.maxpool(conv1[0])], dim=1)# 128X128
+        
+#         dec3 = self.single3(dec3)
+        
+#         dec2 = self.dconv2(dec3)
+#         dec2 = torch.cat([self.upsample(dec2), conv1[0]], dim=1)# 256X256
+#         dec1 = self.dconv1(dec2)
+#         out = self.conv_last(dec1)
 
-        return out
+#         return out
     
 class Unet3plusGlcm(nn.Module):
 
